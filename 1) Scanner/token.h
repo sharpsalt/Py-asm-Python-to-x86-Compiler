1f7c2e04e64ceb37c5082eee132cf9f51048e1fc
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-01-01 05:59:39 +0530

    Extended the usecase by adding operators,brackets,pre,post,loops,while,identation etc

--- 1) Scanner/token.h
+++ 1) Scanner/token.h
@@ -1,29 +1,81 @@
+#include<stdio.h>
+#include<stdbool.h>
+
 #ifndef TOKEN_H
 #define TOKEN_H
+typedef enum {
+    T_EOF,        // End Of File
+    T_INT,        // Integer literal
+    T_FLOAT,      // Floating Point literal
+    T_STRING,     // String literal
+    T_IDENTIFIER, // Variable like x,y etc
+    T_PLUS,       // +
+    T_MINUS,      // -
+    T_STAR,       // *
+    T_SLASH,      // /
+    T_POWER,      // **
+    T_LSHIFT,     // <<
+    T_RSHIFT,     // >>
+    T_BIT_AND,    // &
+    T_BIT_OR,     // |
+    T_BIT_XOR,    // ^
+    T_BIT_NOT,    // ~
+    T_EQ,         // ==
+    T_NE,         // !=
+    T_LT,         // <
+    T_GT,         // >
+    T_LE,         // <=
+    T_GE,         // >=
+    T_ASSIGN,     // =
+    T_PLUS_EQ,    // +=
+    T_MINUS_EQ,   // -=
+    T_STAR_EQ,    // *=
+    T_SLASH_EQ,   // /=
+    T_LSHIFT_EQ,  // <<=
+    T_RSHIFT_EQ,  // >>=
+    T_BIT_AND_EQ, // &=
+    T_BIT_OR_EQ,  // |=
+    T_BIT_XOR_EQ, // ^=
+    T_LPAREN,     // (
+    T_RPAREN,     // )
+    T_LBRACKET,   // [
+    T_RBRACKET,   // ]
+    T_COMMA,      // ,
+    T_COLON,      // :
+    T_IF,         // if
+    T_ELSE,       // else
+    T_WHILE,      // while
+    T_FOR,        // for
+    T_DEF,        // def
+    T_RETURN,     // return
+    T_INPUT,      // input()
+    T_INT_INPUT,  // int(input())
+    T_FLOAT_INPUT,// float(input())
+    T_PRINT,      // print
+    T_NEWLINE,    // Newline (\n)
+    T_INDENT,     // Indentation
+    T_DEDENT,     // Dedentation
+    T_UNKNOWN     // Unknown token
+} TokenType;
 
-#include <stdio.h>   // For FILE*
-
-typedef enum{
-    T_EOF,        //For End Of File
-    T_INT,        //For intvalue
-    T_IDENTIFIER, //For identifier like x,y etc
-    T_PLUS,       //For +
-    T_MINUS,      //For -
-    T_STAR,       //For *
-    T_SLASH,      //For /
-    T_LPAREN,     //For (
-    T_RPAREN,     //For )
-    T_ASSIGN,     //For =
-    T_NEWLINE,    //For Newline (Basically \n)
-    T_UNKNOWN 
-}TokenType;
-
-typedef struct{
+typedef struct {
     TokenType type;
     char* text;
-    int intvalue;
+    union {
+        int intvalue;      //For T_INT
+        double floatvalue; //For T_FLOAT
+    };
+    int line;  //For Soruce line number
+    int column; //For Source column number
 } Token;
 
-Token get_next_token(FILE *source);
+void init_lexer(FILE* source);
+Token get_next_token();
+const char* token_type_to_str(TokenType type);
+
+bool is_operator(TokenType type);
+bool is_bitwise_op(TokenType type);
+bool is_comparison(TokenType type);
+bool is_keyword(TokenType type);
 
 #endif // TOKEN_H
