17ed35894e9f5ac08b974a07fcbd212c472367de
Srijan Verma <bt23cse219@iiitn.ac.in>
2026-03-28 04:10:30 +0530

    fix: Fixed globals helper issue

---
+++ 3) Precedence/globals.h
@@ -0,0 +1,13 @@
+#ifndef GLOBALS_H
+#define GLOBALS_H
+
+#include <stdio.h>
+#include "token.h"
+
+extern FILE *Outfile;
+extern int Line;
+extern int Putback;
+extern FILE *Infile;
+extern Token CurrentToken;
+
+#endif
